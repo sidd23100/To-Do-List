6f097d1c37b801be743e1f16c07ac0e415d8fb69
Siddhant Sharma <siddsharma2310@gmail.com>
2024-05-14 21:05:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const item = document.querySelector("#item")
+const toDoBox = document.querySelector("#to-do-box")
+
+item.addEventListener(
+    "keyup",function(event) {
+        if (event.key == "Enter") {
+            addToDo(this.value)
+            this.value = ""
+        }
+    }
+)
+
+const addToDo = (item) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `
+         ${item}
+        <i class="fas fa-times"></i>
+    `;
+
+    listItem.addEventListener(
+        "click",function () {
+            this.classList.toggle("done");
+        }
+    )
+    listItem.querySelector("i").addEventListener(
+        "click", () =>{
+            listItem.remove()
+        }
+    )
+    toDoBox.appendChild(listItem)
+}
